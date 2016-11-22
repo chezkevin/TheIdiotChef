94b3dc2843a89925d4bc4905ea03ae42972d1932
FH <FH@Fionas-MacBook-Air.local>
2016-11-22 16:25:18 -0600

    working on auth - verification and allowing user that registers to login on registration

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -13,13 +13,20 @@ $(document).ready(function() {
 
 });
 
+// console.log("current USer" + firebase.auth().currentUser);
+
 
 var config = {
-    apiKey: "AIzaSyAVahZhtTXMuf3JFNjcDSSbSFb2EW6MCNI",
-    authDomain: "auth-test-a6dcc.firebaseapp.com",
-    databaseURL: "https://auth-test-a6dcc.firebaseio.com",
-    storageBucket: "auth-test-a6dcc.appspot.com",
-    messagingSenderId: "208122145495"
+    // apiKey: "AIzaSyAVahZhtTXMuf3JFNjcDSSbSFb2EW6MCNI",
+    // authDomain: "auth-test-a6dcc.firebaseapp.com",
+    // databaseURL: "https://auth-test-a6dcc.firebaseio.com",
+    // storageBucket: "auth-test-a6dcc.appspot.com",
+    // messagingSenderId: "208122145495"
+    apiKey: "AIzaSyCfGxrkb9P3oYRWrQ5XL4wxNmpyv_x9VL0",
+    authDomain: "theidiotchef-149717.firebaseapp.com",
+    databaseURL: "https://theidiotchef-149717.firebaseio.com",
+    storageBucket: "theidiotchef-149717.appspot.com",
+    messagingSenderId: "963963795794"
 };
 firebase.initializeApp(config);
 
@@ -32,17 +39,18 @@ let uid = ''; //user id for tracking purposes
 
 
 //button even listners
-$('#btn-guest').on('click', function() {
+// $('#btn-guest').on('click', function() {
 
-    //login anonymously
-    anonymousLogIn();
+//     //login anonymously
+//     anonymousLogIn();
+//     console.log("current USer" + firebase.auth().currentUser);
 
-    //TODO - IF LOGIN ERROR, SHOULD WE NOT MOVE TO NEXT PAGE?
+//     //TODO - IF LOGIN ERROR, SHOULD WE NOT MOVE TO NEXT PAGE?
 
-    //call function to render next page 
-    //TODO
+//     //call function to render next page 
+//     //TODO
 
-});
+// });
 
 $('#register').on('click', function() {
 
@@ -62,6 +70,8 @@ $('#navbar-login-button').on('click', function() {
 
     //login anonymously
     memberLogIn();
+    console.log(firebase.auth().currentUser);
+
 
     //call function to render next page
     //TODO
@@ -87,6 +97,7 @@ $('#navbar-logout-button').on('click', function(e) {
 $('#registration-button').on('click', function() {
 
     registerUser();
+  console.log(firebase.auth().currentUser);
 
     //prevent page refresh
     return false
@@ -146,7 +157,7 @@ function anonymousLogIn() {
 
             //handle login error
         }).catch(e => {
-
+            console.log(e);
             let errorCode = e.code;
             let errorMessage = e.message;
             console.log('anonymous login error: ' + errorMessage);
@@ -187,11 +198,14 @@ function memberLogIn() {
             })
 
             //tell user that they are logged in w email address
+            $('#userName').show();
             $('#userName').html('Logged in as: ' + user.email);
+
 
             //hide login form upon success and display logout button
             $('#navbar-login-form').hide();
-            renderLogOut();
+             $('#register').hide();
+            // renderLogOut();
 
         })
         .then(() => {
@@ -210,10 +224,18 @@ function memberLogIn() {
 
             let errorCode = e.code;
             let errorMessage = e.message;
-            console.log('meber login error: ' + errorMessage);
+              console.log(e);
+            // modal for login messages
+            $('.error-modal').addClass('show');
+            $('.error-modal').removeClass('hide');
+            $('#error-modal-message').html('<p>Login Error: ' + errorMessage + ' Please try again.</p>');
+
+            console.log('Login Error: ' + errorMessage);
 
         });
+
 }
+
 
 function registerUser() {
 
@@ -223,6 +245,7 @@ function registerUser() {
 
     let email = $('#registration-email-input').val().trim();
     let pass = $('#registration-password-input').val().trim();
+
 
     //empty out text that was input
     $('#registration-email-input').val('');
@@ -250,6 +273,9 @@ function registerUser() {
                 lastLogIn: firebase.database.ServerValue.TIMESTAMP
 
             })
+             //tell user that they are logged in w email address
+            $('#userName').show();
+            $('#userName').html('Logged in as: ' + user.email);
 
         })
         .then(() => {
@@ -265,7 +291,7 @@ function registerUser() {
 
             //on success, render next page
             //TODO
-            renderLogOut();
+            // renderLogOut();
 
 
         })
@@ -274,6 +300,13 @@ function registerUser() {
             let errorCode = e.code;
             let errorMessage = e.message;
             console.log('login error: ' + errorMessage);
+            $('.error-modal').addClass('show');
+            $('.error-modal').removeClass('hide');
+            $('#error-modal-message').html('<p>Login Error: ' + errorMessage + ' Please try again.</p>');
+
+            // reset page
+            $('#navbar-login-form').show();
+            $('#register').show();
 
         })
 
@@ -293,7 +326,13 @@ function logOut() {
             console.log('Signed Out');
         })
         .catch(error => {
-            console.error('Sign Out Error: ', error);
+            console.log(error);
+            console.error('Sign Out Error: ', errorMessage);
+            $('.error-modal').addClass('show');
+            $('.error-modal').removeClass('hide');
+            $('#error-modal-message').html('<p>Sign Out Error: ' + errorMessage + '</p>');
+
+
         });
 
     //remove logged in user name in header
@@ -312,6 +351,34 @@ function renderNavBarLogIn() {
 
 }
 
+var currentUser;
+// add a real time listener // firebaseUser null if not logged in
+// this allows user to be logged in after registering
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    console.log(firebaseUser);
+    currentUser = firebaseUser;
+    if (firebaseUser){
+        console.log("user: "+ firebaseUser.email);
+        // btnLogout.show();
+        $('#navbar-logout-button').show();
+        $('#register').hide();
+        $('#navbar-login-form').hide();
+    }
+    else {
+        console.log ('not logged in');
+        // btnLogout.hide();
+        $('#navbar-logout-button').hide();
+        $('#register').show();
+    }
+} )
+
+// close error msg
+$('#close-modal').on('click', function(){
+    // $('.error-modal').hide();
+    console.log("clicking me!");
+    $('.error-modal').removeClass('show');
+    $('.error-modal').addClass('hide');
+})
 /*
 Parking Lot
 	Merge anonymous acount data accumulated upon login w/ user's profile
